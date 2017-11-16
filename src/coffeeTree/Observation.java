0709dee99bc497e09e07cb5e35d2f1f297afbbc5
Jacob Casper <jtcasper@users.noreply.github.com>
2017-11-16 16:44:28 -0500

    Added Observation formatted file parsing (#8)

--- src/coffeeTree/Observation.java
+++ src/coffeeTree/Observation.java
@@ -31,5 +31,30 @@ public class Observation {
 	public void setClassification(String classification) {
 		this.classification = classification;
 	}
+	
+	@Override
+	public boolean equals(Object o) {
+
+		boolean result = false;
+		if (o instanceof Observation) {
+			
+			Observation other = (Observation) o;
+			if((this.getClassification() == null && other.getClassification() == null) || this.getClassification().equals(other.getClassification())) {
+				//TODO This can be cleaned up heavily once Attribute class implemented
+				if (this.getAttributes().length == other.getAttributes().length) {
+					for (int i = 0; i < this.getAttributes().length; i++) {
+						if (this.getAttributes()[i].equals(other.getAttributes()[i])) {
+							result = true;
+						} else {
+							result = false;
+						}
+					}
+				}
+			}
+			
+		}
+		return result;
+		
+	}
 
 }
